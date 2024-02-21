ab015755821ae16ce6fe6f909f338d7678ee813d
Jalen <jalenms2012@gmail.com>
2024-02-20 16:40:24 -0800

    completed userControllers

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -19,7 +19,7 @@ exports.indexUsers = async (req, res) => {
         const users = await User.find()
         res.status(200).json(users)
     } catch (error) {
-        res.status(401).sennd({ message: error.message })
+        res.status(401).send({ message: error.message })
     }
 }
 
@@ -30,6 +30,51 @@ exports.createUser = async (req, res) => {
         const token = await User.generateAuthToken()
         res.json({ user, token })
     } catch (error) {
-        res.status(401).sennd({ message: error.message })
+        res.status(401).send({ message: error.message })
+    }
+}
+
+exports.loginUser = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if(!user || !await bcrypt.compare(req.body.password, user.password)) {
+            res.status(400).send('Invalid login credentials')
+        } 
+        else {
+            const token = await User.generateAuthToken()
+            res.json({ user, token })
+        }
+    } catch (error) {
+        res.status(401).send({ message: error.message })
+    }
+}
+
+exports.indexUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(userItem)
+    } catch (error) {
+        res.status(401).send({ message: error.message })
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        const updates = Object.keys(req.body)
+        const user = await User.findOne({ _id: req.params.id })
+        updates.forEach(update = user[update] = req.body[update])
+        await user.save()
+        res.json({user})
+    } catch (error) {
+        res.status(401).send({ message: error.message })
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        await req.user.deleteOne()
+        res.json({ message: 'User deleted'})
+    } catch (error) {
+       res.status(401).send({ message: error.message }) 
     }
 }
