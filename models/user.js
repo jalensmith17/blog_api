3a67f6be55c9cb4f9a03254a9d1a5a7e33680d7d
Jalen <jalenms2012@gmail.com>
2024-02-21 15:41:31 -0800

    matched instructors code for part 2

--- models/user.js
+++ models/user.js
@@ -1,32 +1,27 @@
 require('dotenv').config()
-const { model, Schema } = require('mongoose')
-const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-
-const blogSchema = new Schema ({
-    name: { required: true, type: String },
-    email: { required: true, type: String },
-    password: { required: true, type: String },
-    posts: [{ type: Schema.Types.ObjectId, ref: 'Blog'}],
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment'}],
-}, {
-    timestamps: true
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Blog'}]
 })
 
 userSchema.pre('save', async function(next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 8)
-    }
-    next()
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8)
+  }
+  next()
 })
 
 userSchema.methods.generateAuthToken = async function() {
-    const secretKey = process.env.JWT_SECRET
-    const token = jwt.sign({ _id: this._id }, secretKey)
-    return token
+  const token = jwt.sign({ _id: this._id }, process.env.SECRET)
+  return token
 }
 
-const User = model('User', userSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
